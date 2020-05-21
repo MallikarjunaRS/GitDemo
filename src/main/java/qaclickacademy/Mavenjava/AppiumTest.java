505c16067319c78caf100cea99e7ada0b5866c4c
HP <mallikarjunrs1992.rymec@gmail.com>
2020-05-21 12:23:21 +0530

    first commit

---
+++ src/main/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,13 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void mobiletest() 
+	{
+		System.out.println("mobiletest");
+	}
+
+}
---
+++ src/main/java/qaclickacademy/Mavenjava/RESTAPI.java
@@ -0,0 +1,19 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPI {
+
+	@Test
+	public void postJira() 
+	{
+		System.out.println("postJira");
+	}
+	
+	@Test
+	public void deletetwitter() 
+	{
+		System.out.println("deletetwitter");
+	}
+	
+}
